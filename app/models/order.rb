956ac042f751556cd21581ac9f015354ba74e3ed
Sergey Lupinos <sslupinos@gmail.com>
2017-03-18 12:25:25 +0400

    Fix order associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :menu
-  has_one :customer, class_name: 'User', foreign_key: 'customer_id'
-  has_one :provider, class_name: 'User', foreign_key: 'provider_id'
+  belongs_to :customer, class_name: 'User'
+  belongs_to :provider, class_name: 'User'
 end
