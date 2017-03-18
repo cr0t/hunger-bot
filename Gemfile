7f4a5c99af0612f068e7bd75d6528a35700593b4
Sergey Lupinos <sslupinos@gmail.com>
2017-03-18 18:20:59 +0400

    Add geocoder gem

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'devise'
 
 gem 'telegram-bot'
 gem 'activeadmin', github: 'activeadmin'
+gem 'geocoder'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   belongs_to :role
 
   after_create :set_role
+  after_validation :geocode
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -9,6 +10,8 @@ class User < ApplicationRecord
   scope :customers, -> { joins(:role).where(roles: { name: 'Customer' }) }
   scope :providers, -> { joins(:role).where(roles: { name: 'Provider' }) }
   scope :admins,    -> { joins(:role).where(roles: { name: 'Admin' }) }
+
+  geocoded_by :address, latitude: :lat, longitude: :lng
 
   def display_name
     "#{name} <#{email}>"
