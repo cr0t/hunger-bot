0f8bfdfb14f9693761ccb73e99b7002590cabcc1
Sergey Lupinos <sslupinos@gmail.com>
2017-03-19 11:07:39 +0400

    Add link to profile on index user's page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,9 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
-    column :email
+    column :email do |user|
+      link_to user.email, admin_user_path(user)
+    end
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
