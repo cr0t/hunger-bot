5a97cf13ac6183b0b3fa2c657f22e2acb0784dcd
Sergey Lupinos <sslupinos@gmail.com>
2017-03-19 12:28:22 +0400

    Fix menu AA object

--- app/admin/menu.rb
+++ app/admin/menu.rb
@@ -1,11 +1,23 @@
 ActiveAdmin.register Menu do
   menu label: 'Menu'
 
+  index title: 'Menu' do
+    selectable_column
+    column :name do |menu|
+      link_to menu.name, admin_menu_path(menu)
+    end
+    column :category
+    column :price
+    column :provider
+    actions
+  end
+
   form do |f|
     f.semantic_errors
     f.inputs "Menu Details" do
-      f.input :category
       f.input :name
+      f.input :price
+      f.input :category
       f.input :provider
     end
     f.actions
