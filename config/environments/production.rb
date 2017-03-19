1683bf06d904dbedfd716c6f24a007c5ad50b7ab
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2017-03-19 11:27:51 +0400

    default_url_options to the production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  routes.default_url_options = { host: 'hunger-bot.datacrafts.io', protocol: 'http' }
 end
